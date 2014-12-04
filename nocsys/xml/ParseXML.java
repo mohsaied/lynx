3dabec5fc1994854d9e79ff3c4e854ae831dda63
mohamed <mohamed@ece.utoronto.ca>
2014-12-04 16:39:38 -0500

    adding readXMLDesign function

--- nocsys/xml/ParseXML.java
+++ nocsys/xml/ParseXML.java
@@ -1,10 +1,12 @@
 package nocsys.xml;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import nocsys.data.Module;
 import nocsys.data.Parameter;
@@ -14,27 +16,30 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class ParseXML {
-    public static void main(String[] args) throws Exception {
+
+    public static List<Module> readXMLDesign(String designPath) throws Exception   {
+        
         // Get the DOM Builder Factory
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        
+
         // Get the DOM Builder
         DocumentBuilder builder = factory.newDocumentBuilder();
 
         // Load and Parse the XML document
-        // document contains the complete XML as a Tree.
-        Document document = builder.parse(ClassLoader.getSystemResourceAsStream("designs/quadratic.xml"));
+        // document contains the complete XML as a Tree
+        Document document = builder.parse(ClassLoader.getSystemResourceAsStream(designPath));
 
         List<Module> modList = new ArrayList<>();
 
-        // Iterating through the nodes and extracting the data.
+        // Iterating through the nodes and extracting the data
         NodeList nodeList = document.getDocumentElement().getChildNodes();
 
         for (int i = 0; i < nodeList.getLength(); i++) {
 
-            // We have encountered an <Module> tag.
+            // We have encountered a <Module> tag
             Node node = nodeList.item(i);
 
             if (node instanceof Element) {
@@ -46,30 +51,41 @@ public class ParseXML {
                 for (int j = 0; j < childNodes.getLength(); j++) {
                     Node cNode = childNodes.item(j);
 
-                    // Identifying the child tag of Module encountered.
+                    // Identifying the child tag of Module encountered
                     if (cNode instanceof Element) {
-                        
-                        switch(cNode.getNodeName()){
+
+                        // child tags can be either ports or parameters
+                        switch (cNode.getNodeName()) {
                         case "parameter":
                             String name = cNode.getAttributes().getNamedItem("name").getNodeValue();
                             String value = cNode.getAttributes().getNamedItem("value").getNodeValue();
-                            Parameter par = new Parameter(name,value);
+                            Parameter par = new Parameter(name, value);
                             mod.addParameter(par);
                             break;
                         case "port":
                             String pname = cNode.getAttributes().getNamedItem("name").getNodeValue();
                             String type = cNode.getAttributes().getNamedItem("type").getNodeValue();
                             int width = Integer.parseInt(cNode.getAttributes().getNamedItem("width").getNodeValue());
-                            Port por = new Port(pname,type,width);
+                            Port por = new Port(pname, type, width);
                             mod.addPort(por);
                             break;
                         }
                     }
                 }
+
+                // add to list of modules in this design
                 modList.add(mod);
             }
         }
+        
+        return modList;
+        
+    }
 
+    public static void main(String[] args) throws Exception {
+
+        List<Module> modList = readXMLDesign("designs/quadratic.xml");
+        
         // Printing the Module list populated.
         System.out.println("Modules:\n");
         for (Module mod : modList) {
