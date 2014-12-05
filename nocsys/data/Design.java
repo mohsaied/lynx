c82471a39be26080e597c53de31bef08691f3203
mohamed <mohamed@ece.utoronto.ca>
2014-12-05 17:02:03 -0500

    Changing implementation of modules collection in Design as a map instead of an array

--- nocsys/data/Design.java
+++ nocsys/data/Design.java
@@ -1,7 +1,9 @@
 package nocsys.data;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 /**
@@ -15,17 +17,17 @@ public class Design {
     private static final Logger log = Logger.getLogger(Design.class.getName());
 
     String name;
-    List<Module> modules;
+    Map<String,Module> modules;
 
     public Design() {
         this.name = null;
-        this.modules = new ArrayList<Module>();
+        this.modules = new HashMap<String,Module>();
         log.info("Creating new design with no name");
     }
 
     public Design(String name) {
         this.name = name;
-        this.modules = new ArrayList<Module>();
+        this.modules = new HashMap<String,Module>();
         log.info("Creating new design: " + name);
     }
 
@@ -37,26 +39,30 @@ public class Design {
         this.name = name;
     }
 
-    public List<Module> getModules() {
+    public Map<String,Module> getModules() {
         return modules;
     }
 
-    public void setModules(List<Module> modules) {
+    public void setModules(Map<String,Module> modules) {
         this.modules = modules;
     }
 
     public void addModule(Module currModule) {
-        this.modules.add(currModule);
+        this.modules.put(currModule.getName(),currModule);
     }
 
     public int getNumModules() {
         return modules.size();
     }
+    
+    public Module getModuleByName(String modName){
+        return this.modules.get(modName);
+    }
 
     @Override
     public String toString() {
         String s = "Design: " + name + "\n\n";
-        for (Module mod : this.modules)
+        for (Module mod : this.modules.values())
             s += mod + "\n";
         return s;
     }
