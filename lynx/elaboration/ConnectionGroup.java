6a0430bccace71d73f1fa1a5aa49ce3039c49a5d
mohamed <mohamed@ece.utoronto.ca>
2015-05-28 16:56:26 -0400

    added elaboration package and moved connectiongroup there - elaboration step will try to infer communication patterns automatically to be able to generate logic that is specific to higher-level protocols (such as multimaster single-slave)

--- lynx/elaboration/ConnectionGroup.java
+++ lynx/elaboration/ConnectionGroup.java
@@ -1,10 +1,12 @@
-package lynx.data;
+package lynx.elaboration;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import lynx.data.Bundle;
+import lynx.data.Connection;
 import lynx.data.MyEnums.ConnectionType;
 
 /**
