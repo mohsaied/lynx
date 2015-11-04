cf48ff408a6be55af4971bd2c3a0424ca0139294
mohamed <mohamed@ece.utoronto.ca>
2015-11-04 13:42:11 -0500

    some progress with output portion of multislave_fast -- but still not 100% functional

--- verilog_common/credit_shells/tm_master_multislave_fast.sv
+++ verilog_common/credit_shells/tm_master_multislave_fast.sv
@@ -11,7 +11,9 @@ module tm_master_multislave_fast
     parameter NUM_CREDITS = 8, //per slave
     parameter ADDRESS_WIDTH = 4,
     parameter VC_ADDRESS_WIDTH = 2,
-	parameter WIDTH_DATA = 36
+    parameter NUM_VC = 4,
+	parameter WIDTH_DATA_OUT = 36,
+	parameter WIDTH_DATA_IN = 36
 )
 (
 	input clk,
@@ -21,8 +23,8 @@ module tm_master_multislave_fast
     input                               send_valid_in,
     output reg                          send_valid_out,
     
-    input            [WIDTH_DATA-1 : 0] send_data_in,
-    output reg       [WIDTH_DATA-1 : 0] send_data_out,
+    input            [WIDTH_DATA_OUT-1 : 0] send_data_in,
+    output reg       [WIDTH_DATA_OUT-1 : 0] send_data_out,
     
     input         [ADDRESS_WIDTH-1 : 0] send_dest_in,
     input         [ADDRESS_WIDTH-1 : 0] send_dest_out,
@@ -35,21 +37,34 @@ module tm_master_multislave_fast
     input                               send_ready_in,  //coming from NoC
     output reg                          send_ready_out, // going to module
     
-    
-    output reg [VC_ADDRESS_WIDTH-1 : 0] curr_ret_vc,  //remove this and replace with ready signals (just for testing)
-    
     //the receiving end tells us when a reply comes back, and we increment the credits
     //we also need to know if it's valid to be able to set the send ready correctly
-    input [3:0] receive_valid
+    input       [NUM_VC-1:0] receive_valid_in,
+    output reg               receive_valid_out,
+    
+    input                    receive_ready_in,  //coming from module
+    output reg [NUM_VC-1: 0] receive_ready_out,  //going to dpkt
+    
+    input      [NUM_VC*WIDTH_DATA_IN-1 : 0] receive_data_in,  //coming from dpkts
+    output reg        [WIDTH_DATA_IN-1 : 0] receive_data_out  //going to module
 );
 
+
+integer i;
+
+//****************************************************************************
+// INBETWEEN PORTION
+//****************************************************************************
+
+reg [VC_ADDRESS_WIDTH-1 : 0] curr_ret_vc;
+reg [VC_ADDRESS_WIDTH-1 : 0] curr_ret_vc_reg;
 
 fifo_da
 #(
     .WIDTH(VC_ADDRESS_WIDTH),
     .DEPTH(2**($clog2(NUM_CREDITS*4))+2) //just has to be bigger than the number of credits
 )
-fifo_inst
+fifo_fast
 (
     .clk(clk),
     .clear(rst),
@@ -57,9 +72,141 @@ fifo_inst
     .i_write_en(send_valid_out),
     .i_full_out(),
     .o_data_out(curr_ret_vc),
-    .o_read_en(|receive_valid),
+    .o_read_en(|receive_valid_in),
     .o_empty_out()
 );
+
+fifo_da
+#(
+    .WIDTH(VC_ADDRESS_WIDTH),
+    .DEPTH(2**($clog2(NUM_CREDITS*4))+2) //just has to be bigger than the number of credits
+)
+fifo_slow
+(
+    .clk(clk),
+    .clear(rst),
+    .i_data_in(send_ret_vc),
+    .i_write_en(send_valid_out),
+    .i_full_out(),
+    .o_data_out(curr_ret_vc_reg),
+    .o_read_en(receive_valid_out),
+    .o_empty_out()
+);
+
+//****************************************************************************
+// RECEIVE PORTION
+//****************************************************************************
+
+//overflow register to hold data
+reg [WIDTH_DATA_OUT-1 : 0] out_buffer [0:NUM_VC-1];
+reg         [NUM_VC-1 : 0] out_buffer_valid;
+reg [WIDTH_DATA_OUT-1 : 0] aux_buffer [0:NUM_VC-1];
+reg         [NUM_VC-1 : 0] aux_buffer_valid;
+
+//demux for ready_out
+always @(*)
+begin
+    for(i = 0; i < NUM_VC; i++)
+    begin
+        if(curr_ret_vc===i)
+        begin
+            receive_ready_out = 0;
+            receive_ready_out[i] = receive_ready_in & ~out_buffer_valid[i];
+        end
+    end //for
+end
+
+//main process to buffer and send out data
+always @ (posedge clk)
+begin
+    if(rst)
+    begin
+        receive_data_out = 0;
+        receive_valid_out = 0;
+        out_buffer_valid = 0;
+        aux_buffer_valid = 0;
+        for(i = 0; i < NUM_VC; i++)
+        begin
+            out_buffer[i] = 0;
+            aux_buffer[i] = 0; 
+        end
+    end
+    else
+    begin
+        
+        //mux incoming data
+        for(i = 0; i < NUM_VC; i++)
+        begin            
+            
+            //-----------------------------------------------
+            // INPUT STAGE
+            //-----------------------------------------------
+            
+            //transfer from aux to out
+            if(aux_buffer_valid[i] & ~out_buffer_valid[i])
+            begin
+                out_buffer[i] = aux_buffer[i];
+                out_buffer_valid[i] = 1;
+                aux_buffer[i] = 0;
+                aux_buffer_valid[i] = 0;
+            end
+            
+            //if we have data coming in
+            if(receive_valid_in[i])
+            begin
+                //first case: out buffer is ready
+                if(~out_buffer_valid[i])
+                begin
+                    out_buffer[i] = receive_data_in[(i+1)*WIDTH_DATA_OUT-1 -: WIDTH_DATA_OUT];
+                    out_buffer_valid[i] = 1;
+                end
+                
+                //out buffer is full but aux buffer is available
+                else if (~aux_buffer_valid)
+                begin
+                    aux_buffer[i] = receive_data_in[(i+1)*WIDTH_DATA_OUT-1 -: WIDTH_DATA_OUT];
+                    aux_buffer_valid[i] = 1;                    
+                end
+                
+                //synposys translate off
+                else if(aux_buffer_valid[i])
+                begin
+                    $display("CAN'T ACCEPT MORE DATA INTO AUX %d!",i);
+                    $stop(1);
+                end
+                //synposys translate on
+            end
+            
+            //-----------------------------------------------
+            // OUTPUT STAGE
+            //-----------------------------------------------
+            
+            //what is happening at the port we want to read from?
+            if(curr_ret_vc_reg===i)
+            begin
+                if(out_buffer_valid[i]) //transfer data from aux buffer and store new data
+                begin
+                    receive_data_out = out_buffer[i];
+                    out_buffer_valid[i] = 0;
+                    receive_valid_out = 1;
+                end
+                
+                else
+                begin
+                    receive_data_out = 0;
+                    receive_valid_out = 0;
+                end
+            end
+            
+            
+        end //for
+        
+    end
+end
+
+//****************************************************************************
+// SEND PORTION
+//****************************************************************************
 
 localparam COUNTER_WIDTH = $clog2(NUM_CREDITS+1)+1;
 
@@ -75,20 +222,17 @@ reg [ADDRESS_WIDTH+VC_ADDRESS_WIDTH-1 : 0] ret_vc_slaveaddr [0:3];
 
 reg ret_vc_assigned;
 
-integer i;
-
 //current dest/vc
 reg [ADDRESS_WIDTH+VC_ADDRESS_WIDTH-1 : 0] sending_dst;
 reg [ADDRESS_WIDTH+VC_ADDRESS_WIDTH-1 : 0] main_buffer_dst;
 reg [ADDRESS_WIDTH+VC_ADDRESS_WIDTH-1 : 0] overflow_buffer_dst;
 
-
 assign send_dest_out = sending_dst[ADDRESS_WIDTH-1:0];
 assign send_vc_out = sending_dst[ADDRESS_WIDTH+VC_ADDRESS_WIDTH-1 -: VC_ADDRESS_WIDTH];
 
 //skid buffer to store the data when we aren't allowed to send it
-reg [WIDTH_DATA-1 : 0] main_buffer;
-reg [WIDTH_DATA-1 : 0] overflow_buffer;
+reg [WIDTH_DATA_OUT-1 : 0] main_buffer;
+reg [WIDTH_DATA_OUT-1 : 0] overflow_buffer;
 //indicates if we have something in the data buffer
 reg main_buffer_valid;
 reg overflow_buffer_valid;
@@ -274,7 +418,7 @@ begin
         //update the outstanding count
         for(i=0; i < 4; i++)
         begin
-            if(receive_valid[i]===1'b1) 
+            if(receive_valid_in[i]===1'b1) 
             begin
                 //synopsys translate off
                 if(ret_vc_count[i] === 0) begin
